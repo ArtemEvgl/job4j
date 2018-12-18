78e739bda42102aeb52ecf631e5492e8ac10a4de
ArtemEvgl <usbhawk5@gmail.com>
2018-12-18 20:35:02 +0400

    JUnit[#98370]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -7,19 +7,19 @@ package ru.job4j.calculate;
 */
 public class Calculate{
 	/**
-	* Method echo.
-	* @param echo Text "aah"
-	* @return echo plus "aah" "aah"
-	*/
-	public String echo(String echo){
-		return String.format("%s %s %s", echo ,echo, echo);
-	}
-	/**
 	* Main
-	* @param args-args
+	* @param args-args.
 	*/
 	public static void main(String[] args){
 	Calculate calc = new Calculate();
 	System.out.println(calc.echo("aah"));
+	}
+	/**
+	* Method echo.
+	* @param name your name.
+	* @return Echo plus your name.
+	*/
+	public String echo(String name){
+		return "Echo, echo, echo : " + name;
 	}
 }
---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+*Test
+*
+*@author Artem Evglevsky
+*@version $Id$
+*@since 0.1
+*/
+public class CalculateTest{
+/**
+*Test echo.
+*/
+@Test
+public void whenTakeNameThenThreeEchoPlusName(){
+	String input="Artem Evglevsky";
+	String expect = "Echo, echo, echo : Artem Evglevsky";
+	Calculate calc = new Calculate();
+	String result = calc.echo(input);
+	assertThat(result,is(expect));
+}
+}
