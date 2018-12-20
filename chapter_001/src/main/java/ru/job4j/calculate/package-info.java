e64b464a8b7159a7cbe3f643d2668802ec5b2941
ArtemEvgl <usbhawk5@gmail.com>
2018-12-20 22:00:21 +0400

    Элементарный калькулятор.[#98360]

--- chapter_001/src/main/java/ru/job4j/calculate/package-info.java
+++
@@ -1,9 +0,0 @@
- 
-/**
-* Package for calculate task.
-*
-* @author Artem Evglevsky
-* @version  $Id$
-* @since 0.1
-*/
-package ru.job4j.calculate;
--- chapter_001/src/test/java/ru/job4j/calculate/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
-* Package for calculateTest task.
-*
-* @author Artem Evglevsky
-* @version  $Id$
-* @since 0.1
-*/
-package ru.job4j.calculate;
