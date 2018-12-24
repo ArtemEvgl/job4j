6942d302eb30f73deb5e6ca0b25ba24d3ce8eba8
ArtemEvgl <usbhawk5@gmail.com>
2018-12-25 01:58:47 +0400

    Рефакторинг кода.[#98339]

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,6 +1,7 @@
 package ru.job4j.loop;
 
 import java.lang.StringBuilder;
+import java.util.function.BiPredicate;
 /**
 * @author Artem Evglevsky.
 * @version $id$
@@ -14,19 +15,11 @@ public class Paint {
 	* @return Рисунок полупирамиды в консоли.
 	*/
 	public String rightTrl(int height) {
-		StringBuilder screen = new StringBuilder();
-		int weight = height;
-		for (int row = 0; row != height; row++) {
-			for (int column = 0; column != weight; column++) {
-				if (row >= column) {
-					screen.append("^");
-				} else {
-					screen.append(" ");
-				}
-			}
-			screen.append(System.lineSeparator());
-		}
-		return screen.toString();
+		return this.loopBy(
+				height,
+				height,
+				(row, column) -> row >= column
+		);		
 	}
 	/**
 	* Метод рисует левосторонюю пирамиду в консоли.
@@ -35,19 +28,11 @@ public class Paint {
 	* @return Рисунок полупирамиды в консоли.
 	*/
 	public String leftTrl(int height) {
-		StringBuilder screen = new StringBuilder();
-		int weight = height;
-		for (int row = 0; row != height; row++) {
-			for (int column = 0; column != weight; column++) {
-				if (row >= weight - column - 1) {
-					screen.append("^");
-				} else {
-					screen.append(" ");
-				}
-			}
-			screen.append(System.lineSeparator());
-		}
-		return screen.toString();
+		return this.loopBy(
+				height,
+				height,
+				(row, column) -> row >= height - column - 1
+		);		
 	}
 	/**
 	* Метод рисует  пирамиду в консоли.
@@ -56,11 +41,26 @@ public class Paint {
 	* @return Рисунок пирамиды в консоли.
 	*/
 	public String pyramid(int height) {
+		return this.loopBy(
+				height,
+				2 * height - 1,
+				(row, column) -> row >= height - column - 1 && row + height - 1 >= column
+		);		
+	}
+	
+	/**
+	* Метод позволяющий произвести рефакторинг кода.
+	*
+	* @param height Высота пирамиды.
+	* @param weight Ширина пирамиды.
+	* @param условие проставление галки.
+	* @return Нарисованную пирамиду в консоли.
+	*/
+	public String loopBy(int height, int weight, BiPredicate<Integer, Integer> predict) {
 		StringBuilder screen = new StringBuilder();
-		int weight = 2 * height - 1;
 		for (int row = 0; row != height; row++) {
 			for (int column = 0; column != weight; column++) {
-				if (row >= height - column - 1 && row + height - 1 >= column) {
+				if (predict.test(row, column)) {
 					screen.append("^");
 				} else {
 					screen.append(" ");
@@ -70,4 +70,5 @@ public class Paint {
 		}
 		return screen.toString();
 	}
+	
 }
