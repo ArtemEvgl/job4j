b5e9997e3b937863a1e9fff262c7ee9034f1325a
ArtemEvgl <usbhawk5@gmail.com>
2019-03-13 20:14:11 +0300

    Реализовать класс Tracker[#98424]

---
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -0,0 +1,86 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class TrackerTest {
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItem() {
+        Tracker tracker = new Tracker();
+        long created = System.currentTimeMillis();
+        Item item = new Item("test1", "testDescription", created);
+        tracker.add(item);
+        Item result = tracker.findById(item.getId());
+        assertThat(result.getName(), is(item.getName()));
+    }
+	
+	@Test
+	public void whenReplaceNameThenReturnNewName() {
+    Tracker tracker = new Tracker();
+    Item previous = new Item("test1", "testDescription", 123L);
+    // Добавляем заявку в трекер. Теперь в объект проинициализирован id.
+    tracker.add(previous);
+    // Создаем новую заявку.
+    Item next = new Item("test2", "testDescription2", 1234L);
+    // Проставляем старый id из previous, который был сгенерирован выше.
+    next.setId(previous.getId());
+    // Обновляем заявку в трекере.
+    tracker.replace(previous.getId(), next);
+    // Проверяем, что заявка с таким id имеет новые имя test2.
+    assertThat(tracker.findById(previous.getId()).getName(), is("test2"));
+	}
+
+    @Test
+    public void testFindId() {
+        Tracker tracker = new Tracker();
+        long created = System.currentTimeMillis();
+        Item item = new Item("test1", "testDescription", created);
+        tracker.add(item);
+        Item result = tracker.findById(item.getId());
+        assertThat(result.getId(), is(item.getId()));
+    }
+
+    @Test
+    public void deleteTest() {
+        Tracker tracker = new Tracker();
+        long created = System.currentTimeMillis();
+        Item item = new Item("test1", "testDescription", created);
+        Item item2 = new Item("test2", "testDescriptio2", created);
+        tracker.add(item);
+        tracker.add(item2);
+        tracker.delete(item.getId());
+        Item item3 = tracker.findById(item.getId());
+        boolean result = false;
+        if (item3 == null) {
+            result = true;
+        }
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void testFindAll() {
+        Tracker tracker = new Tracker();
+        long created = System.currentTimeMillis();
+        Item item = new Item("test1", "testDescription", created);
+        Item item2 = new Item("test2", "testDescriptio2", created);
+        tracker.add(item);
+        tracker.add(item2);
+        Item[] items = tracker.findAll();
+        assertThat(items.length, is(2));
+    }
+
+    @Test
+    public void testFindKey() {
+        Tracker tracker = new Tracker();
+        long created = System.currentTimeMillis();
+        Item item = new Item("test1", "testDescription", created);
+        Item item2 = new Item("test2", "testDescriptio2", created);
+        Item item3 = new Item("test1", "testDescriptio3", created);
+        tracker.add(item);
+        tracker.add(item2);
+        tracker.add(item3);
+        Item[] items = tracker.findByName("test1");
+        assertThat(items[0].getName(), is("test1"));
+    }
+}
