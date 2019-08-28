5031facbca64f2b9bfbeeaa422f80fa2a39c2f24
ArtemEvgl <usbhawk5@gmail.com>
2019-08-28 19:39:24 +0300

    Компаратор для строк.[#98383]

---
+++ chapter_003/src/main/java/sort/ListCompare.java
@@ -0,0 +1,18 @@
+package sort;
+
+import java.util.Comparator;
+
+public class ListCompare implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        int result = 0;
+        int length = left.length() >= right.length() ? right.length() : left.length();
+        for(int i = 0; i < length; i++) {
+            result = Character.compare(left.charAt(i), right.charAt(i));
+            if(result != 0) {
+                break;
+            }
+        }
+        return result !=0 ? result : Integer.compare(left.length(), right.length());
+    }
+}
