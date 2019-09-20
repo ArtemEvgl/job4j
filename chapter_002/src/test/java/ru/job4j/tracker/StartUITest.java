461abcfc6ac39c390fd6e4b7eb74dd160abed9f5
ArtemEvgl <usbhawk5@gmail.com>
2019-09-20 09:43:46 +0300

    В классе StartUI заменить вывод в консоль на Consumer.v2.0

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,16 +1,31 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.function.Consumer;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 
 public class StartUITest {
+	// буфер для результата.
+	private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+	private final Consumer<String> output = new Consumer<String>() {
+		// поле содержит дефолтный вывод в консоль.
+		private final PrintStream stdout = new PrintStream(out);
+		@Override
+		public void accept(String s) {
+			stdout.println(s);
+		}
+	};
 	@Test
 	public void whenUserAddItemThenHasNewItemWithSameName() {
 		Tracker tracker = new Tracker();
 		Input input = new StubInput(new String[]{"1", "test name", "desc", "y"});
-		new StartUI(tracker, input).init();
+		new StartUI(tracker, input, output).init();
 		assertThat(tracker.findAll().get(0).getName(), is("test name"));
 	}
 	
@@ -19,7 +34,7 @@ public class StartUITest {
 		Tracker tracker = new Tracker();
 		Item item = tracker.add(new Item("test name", "decs", System.currentTimeMillis()));
 		Input input = new StubInput(new String[]{"3", item.getId(), "test replace", "заменили заявку", "y"});
-		new StartUI(tracker, input).init();
+		new StartUI(tracker, input, output).init();
 		assertThat(tracker.findById(item.getId()).getName(), is("test replace"));
 	}
 	
@@ -28,7 +43,7 @@ public class StartUITest {
 		Tracker tracker = new Tracker();
 		Item item = tracker.add(new Item("test name", "decs", System.currentTimeMillis()));
 		Input input = new StubInput(new String[]{"4", item.getId(), "y"});
-		new StartUI(tracker, input).init();
+		new StartUI(tracker, input, output).init();
 		boolean result = false;
 		if (tracker.findById(item.getId()) == null) {
 			result = true;
