a3ee8b3da7a937e6f96f5e10e6b3c70a5ffb76a0
ArtemEvgl <usbhawk5@gmail.com>
2019-09-26 15:58:10 +0300

    Крестики-нолики на JavaFX[#98308]

--- chapter_004/src/main/java/addresses/Profiles.java
+++ chapter_004/src/main/java/addresses/Profiles.java
@@ -1,6 +1,5 @@
 package addresses;
 
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,12 +8,7 @@ public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
         List<Address> addresses = profiles.stream().map(profile -> profile.getAddress()).collect(Collectors.toList());
         List<Address> uniqueAddresses = addresses.stream().distinct().collect(Collectors.toList());
-        uniqueAddresses.sort(new Comparator<Address>() {
-            @Override
-            public int compare(Address o1, Address o2) {
-                return o1.getCity().compareTo(o2.getCity());
-            }
-        });
+        uniqueAddresses.sort((o1,o2) -> o1.getCity().compareTo(o1.getCity()));
         return uniqueAddresses;
     }
 
